0c31da14acd7465f7f041b41aa89a3801799b3c9
Lautaro Bonetto <lautaro.bonetto@gmail.com>
2018-04-15 22:35:51 -0300

    Added events y pipelines

---
+++ SC-Abort-SessionEnd/Events/AbortSessionEnd.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Events;
+using System;
+using System.Security.Policy;
+
+namespace SC_Abort_SessionEnd.Events
+{
+  public class AbortSessionEnd
+  {
+    public void OnSessionEnd(object sender, EventArgs args)
+    {
+      CancelingEvent(args);
+    }
+
+    public void OnPostSessionEnd(object sender, EventArgs args)
+    {
+      CancelingEvent(args);
+    }
+
+    private void CancelingEvent(EventArgs args)
+    {
+      ((SitecoreEventArgs)args).Result.Cancel = true;
+      Log.Info("Canceling SessionEnd event", this);
+    }
+  }
+}
---
+++ SC-Abort-SessionEnd/Pipelines/SessionEnd/AbortSessionEnd.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Pipelines.PreprocessRequest;
+
+namespace SC_Abort_SessionEnd.Pipelines.SessionEnd
+{
+  public class AbortSessionEnd : PreprocessRequestProcessor
+  {
+    public override void Process(PreprocessRequestArgs args)
+    {
+      Sitecore.Diagnostics.Assert.ArgumentNotNull(args, "args");
+      args.AbortPipeline();
+      return;
+    }
+  }
+}
